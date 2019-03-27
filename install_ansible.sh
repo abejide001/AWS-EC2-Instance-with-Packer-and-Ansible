e544c01b09364f30858ebb014ea1fde73018ad22
abejide001 <abejidefemi1@gmail.com>
2019-03-27 02:27:39 +0100

    setup ansible implement configuratioin

---
+++ install_ansible.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+function install_ansible() {
+    sudo apt-get update
+    sudo apt-get install software-properties-common -y
+    sudo apt-get repository --yes ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible -y
+}
+install_ansible
